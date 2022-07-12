8f6aab7da2dbba2ad262712c9a56c48028dd2bca
Raj Parekh <rajparekh2294@gmail.com>
2022-07-12 16:32:28 +0530

    Submission

---
+++ expense-tracker/client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ExpenseTracker from './components/ExpenseTracker';
+
+function App() {
+  return (
+    <div>
+      <ExpenseTracker />
+    </div>
+  );
+}
+
+export default App;
